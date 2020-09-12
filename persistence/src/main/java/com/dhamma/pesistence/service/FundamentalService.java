3531f35b4daefae414c20e0ef424d25ad370fbe0
rowanfoo <rowanfoo@gmail.com>
2020-09-12 08:11:34 +0800

    add

--- persistence/src/main/java/com/dhamma/pesistence/service/FundamentalService.java
+++ persistence/src/main/java/com/dhamma/pesistence/service/FundamentalService.java
@@ -14,14 +14,9 @@ public class FundamentalService {
     @Autowired
     FundamentalRepo fundamentalRepo;
 
-//    public Iterable<Fundamental> getAllFundamentalIn(List<String> codes) {
-//        return fundamentalRepo.findAll(QFundamental.fundamental.code.in(codes));
-//    }
-
     public Iterable<Fundamental> codes(List<String> codes) {
         return fundamentalRepo.findAll(QFundamental.fundamental.code.in(codes));
     }
-
 
     public Fundamental code(String code) {
         return fundamentalRepo.findOne(QFundamental.fundamental.code.eq(code)).get();
