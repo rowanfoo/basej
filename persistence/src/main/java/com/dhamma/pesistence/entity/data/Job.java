1f94c2970a8eb071ccfc4339c8d96b35a150564f
rowanfoo <rowanfoo@gmail.com>
2020-10-02 17:37:10 +0800

    add Job

---
+++ persistence/src/main/java/com/dhamma/pesistence/entity/data/Job.java
@@ -0,0 +1,29 @@
+package com.dhamma.pesistence.entity.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Table(name = "History_Indicators",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"message", "date"})
+        }
+)
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String message;
+    public LocalDate date;
+}
