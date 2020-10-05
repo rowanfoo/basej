38ef0f29d1a0d12c3c7b7d19d84998b3119dcebd
rowanfoo <rowanfoo@gmail.com>
2020-10-05 21:37:49 +0800

    add and change user_config

--- persistence/src/main/java/com/dhamma/pesistence/entity/data/User.java
+++ persistence/src/main/java/com/dhamma/pesistence/entity/data/User.java
@@ -1,19 +1,12 @@
 package com.dhamma.pesistence.entity.data;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.google.common.collect.Lists;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.*;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
@@ -42,43 +35,77 @@ public class User {
     @Column(columnDefinition = "jsonb")
     private String algo;
 
-    @JsonIgnore
-    public List<JsonElement> getAlgoAsJsonArray() {
-        return Lists.newArrayList(new Gson().fromJson(algo, JsonArray.class).iterator());
-    }
+    @OneToMany(cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
+    @JoinColumn(name = "userid")
+    List<UserConfig> userConfig;
+
+//    @JsonIgnore
+//    public List<JsonElement> getAlgoAsJsonArray() {
+//        return Lists.newArrayList(new Gson().fromJson(algo, JsonArray.class).iterator());
+//    }
+
+//    @JsonIgnore
+//    public ArrayNode getAlgoAsJsonArrayJackson() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        ArrayNode pp1 = null;
+//        try {
+//            pp1 = objectMapper.readValue(algo, ArrayNode.class);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return pp1;
+//
+//    }
+
+//    @JsonIgnore
+//    public Map<String, List<JsonElement>> getUserConfig() {
+//        Map<String, List<JsonElement>> groupByPriceMap =
+//                getAlgoAsJsonArray().stream().collect(Collectors.groupingBy(a -> {
+//                            return a.getAsJsonObject().get("id").getAsString();
+//                        }
+//
+//                ));
+//        return groupByPriceMap;
+//    }
 
     @JsonIgnore
-    public ArrayNode getAlgoAsJsonArrayJackson() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        ArrayNode pp1 = null;
-        try {
-            pp1 = objectMapper.readValue(algo, ArrayNode.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return pp1;
-
+    public Map<IndicatorType, List<UserConfig>> getUserConfigMap() {
+        Map<IndicatorType, List<UserConfig>> map = userConfig.stream().collect(Collectors.groupingBy(a -> {
+                    return ((UserConfig) a).getType();
+                }
+        ));
+        return map;
     }
+
 
     @JsonIgnore
-    public Map<String, List<JsonElement>> getUserConfig() {
-        Map<String, List<JsonElement>> groupByPriceMap =
-                getAlgoAsJsonArray().stream().collect(Collectors.groupingBy(a -> {
-                            return a.getAsJsonObject().get("id").getAsString();
-                        }
+    public List<UserConfig> getUserConfigType(IndicatorType type) {
+//    public Map<String, List<JsonElement>> getUserConfig(IndicatorType type) {
+//        Map<String, List<JsonElement>> groupByPriceMap =
+//                getAlgoAsJsonArray().stream().collect(Collectors.groupingBy(a -> {
+//                            return a.getAsJsonObject().get("id").getAsString();
+//                        }
+//
+//                ));
+//        return groupByPriceMap;
+//        Map<IndicatorType, List<UserConfig>> map = userConfig.stream().collect(Collectors.groupingBy(a -> {
+//                    return ((UserConfig) a).getType();
+//                }
+//        ));
 
-                ));
-        return groupByPriceMap;
+        return getUserConfigMap().get(type);
     }
 
 
-    private List<JsonElement> getArray(String var) {
-        Map<String, List<JsonElement>> groupByPriceMap =
-                getAlgoAsJsonArray().stream().collect(Collectors.groupingBy(a -> {
-                            return a.getAsJsonObject().get("id").getAsString();
-                        }
+//    private List<JsonElement> getArray(String var) {
+//
+//        Map<String, List<JsonElement>> groupByPriceMap =
+//                getAlgoAsJsonArray().stream().collect(Collectors.groupingBy(a -> {
+//                            return a.getAsJsonObject().get("id").getAsString();
+//                        }
+//                ));
+//        return groupByPriceMap.get(var);
+//    }
 
-                ));
-        return groupByPriceMap.get(var);
-    }
+
 }
---
+++ persistence/src/main/java/com/dhamma/pesistence/entity/data/UserConfig.java
@@ -0,0 +1,56 @@
+package com.dhamma.pesistence.entity.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+@TypeDef(
+        name = "pgsql_enum",
+        typeClass = PostgreSQLEnumType.class
+)
+
+
+public class UserConfig {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    Long userid;
+    LocalDate date;
+    @Enumerated(EnumType.STRING)
+    @Type(type = "pgsql_enum")
+    public IndicatorType type;
+    @Type(type = "jsonb")
+    @Column(columnDefinition = "jsonb")
+    String algo;
+
+
+    @JsonIgnore
+    public String getAlgoValue(String type) {
+        return new Gson().fromJson(algo, JsonObject.class).get(type).getAsString();
+
+    }
+
+    @JsonIgnore
+    public String getAlgoValue() {
+        return getAlgoValue("value");
+    }
+
+
+}
