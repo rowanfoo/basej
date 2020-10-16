529f079aea98eb8652d8a5839e5ad4f3b3851e65
rowanfoo <rowanfoo@gmail.com>
2020-10-16 11:25:44 +0800

    adds pagination

--- persistence/src/main/java/com/dhamma/pesistence/entity/repo/NewsRepo.java
+++ persistence/src/main/java/com/dhamma/pesistence/entity/repo/NewsRepo.java
@@ -1,11 +1,11 @@
 package com.dhamma.pesistence.entity.repo;
 
 import com.dhamma.pesistence.entity.data.News;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface NewsRepo extends JpaRepository<News, Long>, QuerydslPredicateExecutor<News> {
+public interface NewsRepo extends PagingAndSortingRepository<News, Long>, QuerydslPredicateExecutor<News> {
 }
