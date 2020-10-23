d9a0101c6a9929861f8298b1ebcad9a11e44538b
rowanfoo <rowanfoo@gmail.com>
2020-10-23 15:40:34 +0800

    add lombok build

--- persistence/src/main/java/com/dhamma/pesistence/entity/data/Wishlist.java
+++ persistence/src/main/java/com/dhamma/pesistence/entity/data/Wishlist.java
@@ -1,6 +1,9 @@
 package com.dhamma.pesistence.entity.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +11,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Wishlist {
     @Id
