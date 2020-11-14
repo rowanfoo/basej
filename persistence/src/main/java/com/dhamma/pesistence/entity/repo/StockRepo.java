1ff5f8a8fdb2b343cadb5cc78a710381fdb60ac2
rowanfoo <rowanfoo@gmail.com>
2020-11-14 15:04:53 +0800

    add category feature

--- persistence/src/main/java/com/dhamma/pesistence/entity/repo/StockRepo.java
+++ persistence/src/main/java/com/dhamma/pesistence/entity/repo/StockRepo.java
@@ -2,11 +2,22 @@ package com.dhamma.pesistence.entity.repo;
 
 import com.dhamma.pesistence.entity.data.CoreStock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StockRepo extends JpaRepository<CoreStock, Long>, QuerydslPredicateExecutor<CoreStock> {
 
+    @Query("SELECT DISTINCT c.category FROM CoreStock c")
+    public List<String> category();
+
+    @Query("SELECT DISTINCT c.subcategory FROM CoreStock c")
+    public List<String> subcategory();
+
+    @Query("SELECT DISTINCT c.tags FROM CoreStock c")
+    public List<String> tags();
 
 }
