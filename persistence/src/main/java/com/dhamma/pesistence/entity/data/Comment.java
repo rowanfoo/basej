a5a06328094143b4e2f5b14ffe139d2bba3bbd8f
rowanfoo <rowanfoo@gmail.com>
2021-02-06 14:16:28 +0800

    clean up

--- persistence/src/main/java/com/dhamma/pesistence/entity/data/Comment.java
+++ persistence/src/main/java/com/dhamma/pesistence/entity/data/Comment.java
@@ -1,12 +1,22 @@
 package com.dhamma.pesistence.entity.data;
 
+//import com.dhamma.pesistence.entity.data.PeriodType;
+
+import com.dhamma.pesistence.entity.data.type.DurationType;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
 import lombok.Data;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Data
+@TypeDef(
+        name = "pgsql_enum",
+        typeClass = PostgreSQLEnumType.class
+)
 
 public class Comment {
     @Id
@@ -18,7 +28,15 @@ public class Comment {
     public String type; // idea , financial ,
     public String text;
     public String reject;
+//    @Enumerated(EnumType.STRING)
+//    @Type(type = "pgsql_enum")
+//    public PeriodType period;
+
     @Column(columnDefinition = "boolean default false")
     public Boolean isReject = false;
+    @Enumerated(EnumType.STRING)
+    @Type(type = "pgsql_enum")
+    public DurationType period;
+
 
 }
---
+++ persistence/src/main/java/com/dhamma/pesistence/entity/data/type/DurationType.java
@@ -0,0 +1,8 @@
+package com.dhamma.pesistence.entity.data.type;
+
+//this is duration of investment , long term or short
+public enum DurationType {
+    SHORT,
+    LONG,
+    INTERMEDIATE
+}
