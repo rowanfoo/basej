a0eccfbc1e54b9bb4908325775eab6888cd611ee
rowanfoo <rowanfoo@gmail.com>
2021-03-12 13:23:05 +0800

    feature : add paging

--- persistence/src/main/java/com/dhamma/pesistence/entity/repo/HistoryIndicatorsRepo.java
+++ persistence/src/main/java/com/dhamma/pesistence/entity/repo/HistoryIndicatorsRepo.java
@@ -1,10 +1,10 @@
 package com.dhamma.pesistence.entity.repo;
 
 import com.dhamma.pesistence.entity.data.HistoryIndicators;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HistoryIndicatorsRepo extends JpaRepository<HistoryIndicators, Long>, QuerydslPredicateExecutor<HistoryIndicators> {
+public interface HistoryIndicatorsRepo extends PagingAndSortingRepository<HistoryIndicators, Long>, QuerydslPredicateExecutor<HistoryIndicators> {
 }
